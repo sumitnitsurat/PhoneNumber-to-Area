71c83fe7b608a004473610e5abf2d68c76037ce8
sumit-nitsurat <sumit.nitsurat@gmail.com>
2016-06-23 23:12:24 +0530

    Get timezone functionality added .

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var __ = require( 'doublescore' );
+var data = require( './json/data.json' );
+
+var AreaCodes = function() {
+
+};
+
+AreaCodes.prototype.get = function( phoneNumber ) {
+
+	phoneNumber = phoneNumber.replace( /^\+?[10]/, '' ).replace( /[^0-9]/g, '' ).match( /^([0-9]{3})/ );
+
+	if ( !phoneNumber ) {
+		return new Error( 'phone number not valid' );
+	}
+
+	phoneNumber = phoneNumber[ 1 ];
+
+	if ( data.hasOwnProperty( phoneNumber ) ) {
+		return __( data[ phoneNumber ] ).clone();
+	}
+
+	return new Error( 'not found' );
+
+};
+
+
+module.exports = AreaCodes;
